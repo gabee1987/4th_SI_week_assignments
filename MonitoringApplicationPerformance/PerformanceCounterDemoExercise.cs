ecede0a477e7320bea43b5a5a0de84244b4535ff
gabee1987 <gabee1987@gmail.com>
2017-12-07 14:18:10 +0100

    Instrumentation Hands-on - Exercise 3 is done

---
+++ MonitoringApplicationPerformance/PerformanceCounterDemoExercise.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.Timers;
+
+namespace MonitoringApplicationPerformance
+{
+    class PerformanceCounterDemoExercise
+    {
+        private static PerformanceCounter HeapCounter = null;
+        private static PerformanceCounter ExceptionCounter = null;
+        private static Timer DemoTimer;
+
+
+        static void Main(string[] args)
+        {
+            DemoTimer = new Timer(3000);
+            DemoTimer.Elapsed += new ElapsedEventHandler(OnTick);
+            DemoTimer.Enabled = true;
+
+            HeapCounter = new PerformanceCounter(".NET CLR Memory", "# Bytes in all Heaps");
+            HeapCounter.InstanceName = "_Global_";
+
+            ExceptionCounter = new PerformanceCounter(".NET CLR Exceptions", "# of Exceps Thrown");
+            ExceptionCounter.InstanceName = "_Global_";
+
+            Console.WriteLine("Press [Enter] to Quit Program");
+            Console.ReadLine();
+
+            Console.WriteLine("# of Bytes in all Heaps : " + HeapCounter.NextValue().ToString());
+            Console.WriteLine("# of Framework Exceptions Thrown : " + ExceptionCounter.NextValue().ToString());
+
+            Console.ReadLine();
+        }
+
+        private static void OnTick(object source, ElapsedEventArgs e)
+        {
+        }
+    }
+}
