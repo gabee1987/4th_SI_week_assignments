ce6d47699d0c9f40a7ea62353b2b8d51678839fe
gabee1987 <gabee1987@gmail.com>
2017-12-07 11:13:07 +0100

    Reflection Hands-on - Exercise 2 is done

---
+++ AssemblyAttrDemo/AssemblyAttrDemoExercise.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+
+namespace AssemblyAttrDemo
+{
+    class AssemblyAttrDemoExercise
+    {
+        static void Main(string[] args)
+        {
+            Assembly a = Assembly.GetExecutingAssembly();
+            Type attrType = typeof(AssemblyDescriptionAttribute);
+            object[] attrs = a.GetCustomAttributes(attrType, false);
+
+            if (attrs.Length > 0)
+            {
+                AssemblyDescriptionAttribute desc = (AssemblyDescriptionAttribute)attrs[0];
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Description is:  ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(desc.Description);
+                Console.ResetColor();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
